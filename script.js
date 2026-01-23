751f51c5d53e10d3a7b552720393808bccc25b1a
thigoai <tfreire352@gmail.com>
2026-01-23 10:24:32 -0300

    fix: keyboard shortcuts

--- script.js
+++ script.js
@@ -78,6 +78,6 @@ function print_pdf() {
 window.addEventListener('keydown', e => {
     if (e.ctrlKey && e.key === 's' || e.key === 'S') { e.preventDefault(); save_file(); }
     if (e.ctrlKey && e.key === 'o' || e.key === 'O') { e.preventDefault(); open_file(); }
-    if (e.ctrlKey && e.key === 'p' || e.key === 'P') { e.preventDefault(); open_file(); }
-    if (e.ctrlKey && e.key === 'h' || e.key === 'H') { e.preventDefault(); togglePreview(); }
+    if (e.ctrlKey && e.key === 'p' || e.key === 'P') { e.preventDefault(); print_pdf(); }
+    if (e.ctrlKey && e.key === 'v' || e.key === 'V') { e.preventDefault(); togglePreview(); }
 });
