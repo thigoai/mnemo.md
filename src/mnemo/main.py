c97e90f4803a8be386a3b93dfe5603500aacd094
thigoai <tfreire352@gmail.com>
2026-02-03 21:07:20 -0300

    refactor: New Folder Structure

--- src/mnemo/main.py
+++ src/mnemo/main.py
@@ -12,7 +12,7 @@ def get_resource_path(relative_path):
         return os.path.join(sys._MEIPASS, relative_path)
 
     base_path = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(base_path, relative_path)
+    return os.path.normpath(os.path.join(base_path, "..", "..", "frontend", relative_path))
 
 
 class Api:
